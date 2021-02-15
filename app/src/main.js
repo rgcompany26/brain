e7b26b748aaf8a4eef00f97f194e4f69ad6c12f4
Ruben Abarca <ruge0326@gmail.com>
2021-02-14 22:59:32 -0500

    clean home

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,17 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
 createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+	.use(store)
+	.use(router)
+	.use(PrimeVue)
+	.component("Button", Button)
+	.mount("#app");
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Question msg=""/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Question from "@/components/Question.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Question
   }
 };
 </script>
